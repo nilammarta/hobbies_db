a312f69a5264abe4eaacba0c1819347ed401d6a6
Nilam <nilammartaanggreni@gmail.com>
2023-11-06 10:50:15 +0800

    no 5

--- hobbies_query.sql
+++ hobbies_query.sql
@@ -48,3 +48,24 @@ FROM persons p
 WHERE p.id IS NULL;
 
 
+# no: 5
+SELECT IFNULL(p.id, '-')   AS 'Person ID',
+       IFNULL(p.name, '-') AS Name,
+       IFNULL(GROUP_CONCAT(h.name), '-')  AS Hobi
+
+FROM persons p
+         LEFT JOIN person_hobbies ph ON p.id = ph.person_id
+         LEFT JOIN hobbies h ON ph.hobby_id = h.id
+GROUP BY p.id
+UNION
+SELECT IFNULL(p.id, '-')   AS 'Person ID',
+       IFNULL(p.name, '-') AS Name,
+       IFNULL(GROUP_CONCAT(h.name), '-')  AS Hobi
+FROM persons p
+         RIGHT JOIN person_hobbies ph ON p.id = ph.person_id
+         RIGHT JOIN hobbies h ON ph.hobby_id = h.id
+WHERE p.id IS NULL
+GROUP BY p.id
+ORDER BY `Person ID` ASC;
+
+
